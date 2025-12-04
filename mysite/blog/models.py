6bb47339a3fe7d5cf3c72fff19854608f5ee279a
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-12-04 14:22:42 +0330

    Refactor blog templates, add pagination, and improve post URL structure
    - Added pagination support to post_list view and templates
    - Updated Post model: added get_absolute_url and unique_for_date slug
    - Refactored post detail and list templates to extend base.html
    - Added new base.html and pagination.html templates
    - Updated URLs to use date-based slug routes
    - Updated CSS to include pagination styles
    - Fixed post detail lookup to use slug + publish date components

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -15,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date="publish")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_post"
     )
@@ -35,3 +36,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.Published.all()
+    post_list = Post.Published.all()
+    # Show 3 contacts per page.
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get("page")
+    try:
+        posts = paginator.get_page(page_number)
+    # Raises if the number cannot be converted to an integer
+    except PageNotAnInteger:
+        posts = paginator.get_page(1)
+    # Raises EmptyPage if the given page number doesn’t exist
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
